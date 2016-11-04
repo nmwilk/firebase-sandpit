bce8dfb46bbcfa9f34798eed2f7edfcc60c8f44f
Neil Wilkinson <neil.wilkinson@sage.com>
2016-11-04 14:12:10 +0000

    Added remote config to change action bar color.

--- app/src/main/java/com/sageone/firebasesandpit/MainActivity.kt
+++ app/src/main/java/com/sageone/firebasesandpit/MainActivity.kt
@@ -2,14 +2,19 @@ package com.sageone.firebasesandpit
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.Menu
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firebaseRemoteConfig: FirebaseRemoteConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,23 @@ class MainActivity : AppCompatActivity() {
 
         transactionsList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         transactionsList.adapter = TransactionsAdapter(this)
+
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        firebaseRemoteConfig.setConfigSettings(FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(BuildConfig.DEBUG).build())
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        firebaseRemoteConfig.fetch(0).addOnCompleteListener(this) {
+            Log.d(TAG, "firebase remote config completion, success ${it.isSuccessful}")
+            if (it.isSuccessful) {
+                firebaseRemoteConfig.activateFetched()
+                val colorHexString = firebaseRemoteConfig.getString("themeColor")
+                val argb = java.lang.Long.parseLong(colorHexString, 16).toInt()
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(argb))
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -45,6 +67,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         val REQUEST_CODE_NEW = 100
 
-        val TAG = MainActivity::class.java.simpleName
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
